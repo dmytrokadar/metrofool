06dbe1125673eb50938123c318c7c9896db4d180
Dmytro Kadar <dmytrokadar@gmail.com>
2024-03-29 19:28:43 +0100

    Camera follows player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,13 @@ public class Player : MonoBehaviour
 {
     private Vector2 movement;
     private Rigidbody rb;
+    [SerializeField] private float camOffset = -7;
     [SerializeField] private float moveSpeed = 1;
     [SerializeField] private float rotationSpeed = 1;
+    [SerializeField] private GameObject cam;
 
     void Start(){
+        camOffset = cam.transform.position.x;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -20,11 +23,12 @@ public class Player : MonoBehaviour
 
     void FixedUpdate(){
         Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, movement.x, 0) * rotationSpeed * Time.fixedDeltaTime);
-        // Vector3 deltaRotation = new Vector3(0, movement.x, 0) * rotationSpeed *Time.fixedDeltaTime;
         rb.MoveRotation(rb.rotation * deltaRotation);
+
         Vector3 moveDirection = new Vector3(movement.y, 0, movement.y);
-        // moveDirection = transform.TransformDirection(moveDirection);
         moveDirection = rb.rotation * moveDirection;
         rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+
+        cam.transform.position = new Vector3(rb.position.x + camOffset, cam.transform.position.y, rb.position.z + camOffset);
     }
 }
