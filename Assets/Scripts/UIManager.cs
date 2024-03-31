293b60991255a1a47c45eab1f1649a30156b3d1a
Dmytro Kadar <dmytrokadar@gmail.com>
2024-03-31 01:49:53 +0100

    added map

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,13 +7,13 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public void OnClickExit(){
-        if (Application.isEditor)
-            EditorApplication.ExitPlaymode();
-        else
+        // if (Application.isEditor)
+        //     EditorApplication.ExitPlaymode();
+        // else
             Application.Quit();
     }
 
     public void ChangeScene(string scName){
-        SceneManager.LoadScene(scName, LoadSceneMode.Additive);
+        SceneManager.LoadScene(scName, LoadSceneMode.Single);
     }
 }
